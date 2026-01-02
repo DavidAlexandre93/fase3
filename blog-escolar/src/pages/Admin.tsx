b4d9a378b1cdc29aee0eed78c10dd71fa854a64f
caroline <caroline.sant91@gmail.com>
2026-01-01 23:05:17 -0500

    Admin.tsx: estrutura inicial do painel administrativo

---
+++ blog-escolar/src/pages/Admin.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Admin: React.FC = () => {
+  // Aqui você vai buscar e exibir a lista de posts, com botões para editar/excluir
+  return (
+    <div>
+      <h1>Painel Administrativo</h1>
+      {/* Lista de posts com botões de editar/excluir */}
+    </div>
+  );
+};
+
+export default Admin;
